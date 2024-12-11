893cac932d4098c91135f866fbd4a11e6c72ca61
Tariq <tariqpasha04@gmail.com>
2024-12-11 23:09:59 +0530

    updates added

--- blueticks/settings.py
+++ blueticks/settings.py
@@ -82,13 +82,22 @@ WSGI_APPLICATION = 'blueticks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialize',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'database-1.cd080muocsth.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',  # Default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -135,3 +144,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
